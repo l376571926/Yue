162205b493414906258052c5c2d16845e48011cc
李艺为 <15897461476@139.com>
2018-09-24 23:02:35 +0800

    引用自定义excel解析sdk

--- app/src/main/java/group/tonight/yue/HomeActivity.java
+++ app/src/main/java/group/tonight/yue/HomeActivity.java
@@ -22,10 +22,14 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.socks.library.KLog;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+
+import jxl.read.biff.BiffException;
 
 public class HomeActivity extends AppCompatActivity {
 
--- app/src/main/java/group/tonight/yue/MainActivity.java
+++ app/src/main/java/group/tonight/yue/MainActivity.java
@@ -10,19 +10,10 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.socks.library.KLog;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
-import jxl.Cell;
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
+import group.tonight.workbookhelper.WorkbookHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,73 +34,29 @@ public class MainActivity extends AppCompatActivity {
                     .putBoolean("first_launch", false)
                     .apply();
 
-            new Thread(new Runnable() {
+
+            WorkbookHelper helper = new WorkbookHelper(this);
+            helper.setFileName("深圳.xls");
+            helper.setKeyRowIndex(1);
+            helper.setKeyArray(getResources().getStringArray(R.array.work_book_key_entries));
+            helper.parse(new WorkbookHelper.OnParseResultListener() {
                 @Override
-                public void run() {
-
-                    try {
-                        Workbook workbook = Workbook.getWorkbook(getAssets().open("深圳.xls"));
-                        int numberOfSheets = workbook.getNumberOfSheets();
-                        if (numberOfSheets != 0) {
-                            Sheet sheet = workbook.getSheet(0);
-                            int rows = sheet.getRows();//获取总共多少行
-                            int columns = sheet.getColumns();//获取总共多少列
-                            List<String> keyList = new ArrayList<>();
-//                            keyList.add("girlId");
-                            keyList.add("title");
-                            keyList.add("type");
-                            keyList.add("area");
-                            keyList.add("address");
-                            keyList.add("from");
-                            keyList.add("count");
-                            keyList.add("ageRange");
-                            keyList.add("suZhi");
-                            keyList.add("score");
-                            keyList.add("serviceItems");
-                            keyList.add("price");
-                            keyList.add("openTime");
-                            keyList.add("surroundings");
-                            keyList.add("safety");
-                            keyList.add("contactInfo");
-                            keyList.add("evaluate");
-                            keyList.add("description");
-
-
-                            JSONArray jsonArray = new JSONArray();
-                            for (int m = 0; m < rows; m++) {
-                                if (m == 0) {
-                                    continue;
-                                }
-                                if (m == 1) {
-                                    continue;
-                                }
-                                //循环读取第i行的所有列的数据
-                                JSONObject jsonObject = new JSONObject();
-                                for (int n = 0; n < columns; n++) {
-                                    //sheet.getCell(列，行);
-                                    Cell cell = sheet.getCell(n, m);
-                                    String contents = cell.getContents();
-                                    jsonObject.put(keyList.get(n), contents);
-                                }
-                                jsonArray.put(jsonObject);
-                            }
-
-                            Type type = new TypeToken<List<Girl>>() {
-                            }.getType();
-                            String json = jsonArray.toString();
-                            List<Girl> beanList = new Gson().fromJson(json, type);
-
-                            UserDatabase.get().getGirlDao().insert(beanList);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (BiffException e) {
-                        e.printStackTrace();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                public void onError(Exception e) {
+                    KLog.e(e);
                 }
-            }).start();
+
+                @Override
+                public void onResult(String json) {
+                    KLog.e(json);
+
+                    Type type = new TypeToken<List<Girl>>() {
+                    }.getType();
+                    List<Girl> beanList = new Gson().fromJson(json, type);
+                    KLog.e();
+
+                    UserDatabase.get().getGirlDao().insert(beanList);
+                }
+            });
         } else {
 
         }
